40ed5296d68e37e3777cd89417956b2a876f48c2
unknown <piljaycee@gmail.com>
2022-01-12 19:34:52 +0700

    install views

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
+const expressLayout = require('express-ejs-layouts')
 const { PORT = 8000} = process.env
 
-
-// View Enigine 
+// View Engine 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
+app.use(expressLayout)
 
 app.use(express.urlencoded({extended:flase}))
 app.use(express.json())
